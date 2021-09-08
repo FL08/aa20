781280b9becf8eba4bd16c11f9c73055e79dffc6
FL08 <48310058+FL08@users.noreply.github.com>
2021-09-08 17:22:44 +0900

    Add files via upload

---
+++ aa20-rpt02/start/hello_function.js
@@ -0,0 +1,4 @@
+//hello_function.js
+module.export = function(what){
+    console.log("Hello " + what + " !");
+}
---
+++ aa20-rpt02/start/index_aa20.js
@@ -0,0 +1,7 @@
+//index_aann.js
+
+var myinfo = require('./aa20info');
+
+myinfo("aa20", "FL08", '010-4242-7878');
+
+myinfo("aa20", "Comsi", '010-6262-9696')
